c7b695cb31e860d949ba41ae0ab71fc511996b65
Scott Prue <scott@residenetwork.com>
2017-07-29 04:32:39 -0700

    Fix tests and lint ignoring.

--- tests/routes/Projects/routes/Project/components/NewInstanceDialog/NewInstanceDialog.spec.js
+++ tests/routes/Projects/routes/Project/components/NewInstanceDialog/NewInstanceDialog.spec.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { NewInstanceDialog } from 'components/NewInstanceDialog/NewInstanceDialog'
+import NewInstanceDialog from 'routes/Projects/routes/Project/components/NewInstanceDialog'
 import { shallow } from 'enzyme'
 
 describe('(Component) NewInstanceDialog', () => {
   let _component
 
   beforeEach(() => {
-    _component = shallow(<NewInstanceDialog />)
+    _component = shallow(
+      <NewInstanceDialog
+        open
+        onRequestClose={() => console.log('request close')} // eslint-disable-line no-console
+      />
+    )
   })
 
   it('Renders div', () => {
